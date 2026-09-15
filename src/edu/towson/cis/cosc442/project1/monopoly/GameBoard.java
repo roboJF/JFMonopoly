1cd9cbf013a636d2501818162f5985c54687b884
hoboJF <jadenfiero@gmail.com>
2026-09-15 17:20:09 -0400

    Task 11 - Refactored cell.getColorGroup() calls into its own variable, as well as adding javadocs to GameBoard.java

--- src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
@@ -10,11 +10,18 @@ public class GameBoard {
 	//the key of colorGroups is the name of the color group.
 	private Hashtable<String, Integer> colorGroups = new Hashtable<String, Integer>();
 	private ArrayList<Card> communityChestCards = new ArrayList<Card>();
+	/**
+	 * Constructs a new GameBoard and initializes it with a Go cell.
+	 */
 	public GameBoard() {
 		Cell go = new GoCell();
 		addCell(go);
 	}
 
+    /**
+     * Adds a card to the appropriate deck based on its card type.
+     * @param card the Card to add to the community chest or chance deck
+     */
     public void addCard(Card card) {
         if(card.getCardType() == Card.TYPE_CC) {
             communityChestCards.add(card);
@@ -23,16 +30,29 @@ public class GameBoard {
         }
     }
 	
+	/**
+	 * Adds a general cell to the game board.
+	 * @param cell the Cell to add to the board
+	 */
 	public void addCell(Cell cell) {
 		cells.add(cell);
 	}
 	
+	/**
+	 * Adds a property cell to the board and updates the count of properties in its color group.
+	 * @param cell the PropertyCell to add to the board
+	 */
 	public void addCell(PropertyCell cell) {
-		int propertyNumber = getPropertyNumberForColor(cell.getColorGroup());
-		colorGroups.put(cell.getColorGroup(), new Integer(propertyNumber + 1));
+		String colorGroup = cell.getColorGroup();
+		int propertyNumber = getPropertyNumberForColor(colorGroup);
+		colorGroups.put(colorGroup, new Integer(propertyNumber + 1));
         cells.add(cell);
 	}
 
+    /**
+     * Draws and returns the first community chest card, then returns it to the deck.
+     * @return the drawn community chest Card
+     */
     public Card drawCCCard() {
         Card card = (Card)communityChestCards.get(0);
         communityChestCards.remove(0);
@@ -40,6 +60,10 @@ public class GameBoard {
         return card;
     }
 
+    /**
+     * Draws and returns the first chance card, then returns it to the deck.
+     * @return the drawn chance Card
+     */
     public Card drawChanceCard() {
         Card card = (Card)chanceCards.get(0);
         chanceCards.remove(0);
@@ -47,14 +71,28 @@ public class GameBoard {
         return card;
     }
 
+	/**
+	 * Returns the cell at the specified index in the board.
+	 * @param newIndex the index of the Cell to retrieve
+	 * @return the Cell at the given index
+	 */
 	public Cell getCell(int newIndex) {
 		return (Cell)cells.get(newIndex);
 	}
 	
+	/**
+	 * Returns the total number of cells on the game board.
+	 * @return the count of cells on the board
+	 */
 	public int getCellNumber() {
 		return cells.size();
 	}
 	
+	/**
+	 * Returns all property cells in the specified color group monopoloy.
+	 * @param color the color group name
+	 * @return an array of PropertyCell objects in the monopoly color group
+	 */
 	public PropertyCell[] getPropertiesInMonopoly(String color) {
 		PropertyCell[] monopolyCells = 
 			new PropertyCell[getPropertyNumberForColor(color)];
@@ -72,6 +110,11 @@ public class GameBoard {
 		return monopolyCells;
 	}
 	
+	/**
+	 * Returns the number of properties in the given color group.
+	 * @param name the color group name
+	 * @return the count of properties in the color group
+	 */
 	public int getPropertyNumberForColor(String name) {
 		Integer number = (Integer)colorGroups.get(name);
 		if(number != null) {
@@ -80,6 +123,11 @@ public class GameBoard {
 		return 0;
 	}
 
+	/**
+	 * Finds and returns the cell with the specified name.
+	 * @param string the name of the Cell to find
+	 * @return the matching Cell if found; otherwise null
+	 */
 	public Cell queryCell(String string) {
 		for(int i = 0; i < cells.size(); i++){
 			Cell temp = (Cell)cells.get(i); 
@@ -90,6 +138,11 @@ public class GameBoard {
 		return null;
 	}
 	
+	/**
+	 * Finds and returns the index of the cell with the specified name.
+	 * @param string the name of the Cell to find
+	 * @return the index of the matching Cell or -1 if not found
+	 */
 	public int queryCellIndex(String string){
 		for(int i = 0; i < cells.size(); i++){
 			Cell temp = (Cell)cells.get(i); 
@@ -100,6 +153,9 @@ public class GameBoard {
 		return -1;
 	}
 
+    /**
+     * Removes all community chest cards from the deck.
+     */
     public void removeCards() {
         communityChestCards.clear();
     }
