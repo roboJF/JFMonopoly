8e46ce895fef7750b37c1313952e6b0dcafff808
hoboJF <jadenfiero@gmail.com>
2026-09-15 19:01:42 -0400

    Task 13 - Exported preliminary javadocs that (i believe) are correct

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.towson.cis.cosc442.project1.monopoly"},{"l":"edu.towson.cis.cosc442.project1.monopoly.gui"}];updateSearchResults();
